9a29b4ff5dd248e646ff98e16b5ca20c399f2ea8
Brent Lombaard <lombaardbrent@gmail.com>
2023-02-06 07:47:36 +0200

    NavBar Added

---
+++ next-tutorial/app/page.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data: { id: number; title: string }[] = await getPosts();
+
+  return (
+    <main className='py-4 px-48'>
+      <Link
+        href='dashboard'
+        className='bg-teal-700 text-black font-medium py-2 px-4 rounded-md'
+      >
+        Go to dashboard
+      </Link>
+      {data.map((post) => (
+        <h1 className='text-lg py-6'>{post.title}</h1>
+      ))}
+    </main>
+  );
+}
